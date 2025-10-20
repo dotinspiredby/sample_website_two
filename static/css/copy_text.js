29f21df9498b733f4f94d29a6ea1f5cf628c7786
Katharina Meldre <imkacello@gmail.com>
2025-10-20 05:25:55 +0300

    upd_20_10_2025

--- static/css/copy_text.js
+++ static/css/copy_text.js
@@ -1,6 +1,11 @@
 function copyText(id) {
     var text = document.getElementById(id);
-    navigator.clipboard.writeText(text.innerText);
-    alert("Copied To Clipboard");
+    navigator.clipboard.writeText(text.innerText)
+    .then(() => {
+        alert("Copied successfully");
+      })
+      .catch(() => {
+        alert("Error");
+      });
 }
 
